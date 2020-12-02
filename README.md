3ae56ba220c5db8bb948ef989c838aa2e8e44dd9
Stephen Ingram <frowey@gmail.com>
2020-12-02 04:57:47 -0500

    Day 1 complete

--- README.md
+++ README.md
@@ -8,3 +8,19 @@ It's been a goal of mine to start building some Rust muscle memory,
 and nothing like an unpaid sabbatical and a bunch of exercises to make
 it happen.
 
+## Day 1
+
+The hardest part of this exercise was just figuring out how the heck to read in a file and create a list of integers from the lines of the file.
+
+### Part 1
+
+Naive solution: O(N^2) iterate over all distinct pairs
+
+Since the size of the input is small (200 lines), this seems fine to me.  I'm using iterators to get more familiar with them.  They remind me of Python comprehensions and Java Streams.
+
+### Part 2
+
+Again, the size of the input is small enough that just looping over distinct triples is going to be instantaneous.
+
+I had some trouble here keeping track of the unwrapping.
+
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+
+fn main() -> io::Result<()> {
+
+    // read input
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    // parse lines
+    let values: Vec<i32> = reader.lines()
+        .map(|l| l.unwrap().parse::<i32>().unwrap())
+        .collect();
+
+    // iterate over all pairs of values
+    let (a, b) = values[0..values.len()-1].iter()
+        .enumerate()
+        .map(|(i, x)| values[i+1..values.len()].iter()
+                        .map(|y| (x, y))
+                        .filter(|z| z.0+z.1 == 2020).next())
+        .filter(|v| matches!(v, Some((_, _))))
+        .map(|v| v.unwrap())
+        .next().unwrap();
+    println!("Part 1: {} + {} == 2020, a * b == {}", a, b, a*b);
+
+    // iterate over all triples of values
+    let (a, b, c) = values[0..values.len()-2].iter()
+        .enumerate()
+        .map(|(i, x)| values[i+1..values.len()-1].iter()
+            .enumerate()
+            .map(|(j, y)| values[i+j+1..values.len()].iter()
+                .map(|z| (x, y, z))
+                .filter(|z| z.0+z.1+z.2 == 2020)
+                .next()
+            )
+            .filter(|v| matches!(v, Some((_, _, _))))
+            .map(|v| v.unwrap())
+            .next()
+        )
+        .filter(|v| matches!(v, Some((_, _, _))))
+        .map(|v| v.unwrap())
+        .next().unwrap();
+    println!("Part 2: {} + {} + {} == 2020, a * b * c == {}", a, b, c, a*b*c);
+    return Ok(())
 }
